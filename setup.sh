9add7e07c9356a88dab9f8950062fe0828cb1f16
Karol Krizka <karol.krizka@cern.ch>
2021-02-11 19:44:56 +0000

    Add build CI

---
+++ setup.sh
@@ -0,0 +1,28 @@
+if [ ${#} != 0 ] && [ ${#} != 1 ]; then
+    echo "usage: ${0} [build]"
+    return 1
+fi
+
+#
+# Check for build directory
+MYBUILD=build
+if [ ${#} == 1 ]; then
+    MYBUILD=${1}
+fi
+
+if [ ! -d ${MYBUILD} ]; then
+    echo "Build directory ${MYBUILD} does not exist!"
+    return 1
+fi
+
+# Convert to absolute path
+MYBUILD=$(realpath ${MYBUILD})
+
+#
+# Main software
+source /opt/ilcsoft/muonc/init_ilcsoft.sh
+
+#
+# Add exts
+LD_LIBRARY_PATH="$(find ${MYBUILD}/exts/*/lib -type d | tr '\n' ';')${LD_LIBRARY_PATH}"
+PATH="$(find ${MYBUILD}/exts/*/bin -type d | tr '\n' ';')${PATH}"
