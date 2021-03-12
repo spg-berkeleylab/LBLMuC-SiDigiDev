60e665303271e374b46ce3b3aebc31b29d51e0e1
Simone Pagan Griso <simone.pagan.griso@cern.ch>
2021-03-12 13:12:02 -0800

    removed ACTS and its dependencies for now, adjust setup.sh to update MARLIN_DLL

--- setup.sh
+++ setup.sh
@@ -27,3 +27,17 @@ source /opt/ilcsoft/muonc/init_ilcsoft.sh
 # Add exts
 export LD_LIBRARY_PATH="$(find ${MYBUILD}/exts/* -name lib64 -type d | tr '\n' ':')$(find ${MYBUILD}/exts/* -name lib -type d | tr '\n' ':')${LD_LIBRARY_PATH}"
 export PATH="$(find ${MYBUILD}/exts/*/bin -type d | tr '\n' ':')${PATH}"
+
+# Modify MARLIN_DLL for packages
+for pkg in `ls -1 -d packages/* | tr '\n' '\0' | xargs -0 -n 1 basename`; do
+    # check if package is in MARLIN_DLL
+    FOUND=0
+    echo ${MARLIN_DLL} | grep '\(:\|^\)\([^:]*\/'$pkg'\/[^:]*\)\(:\|$\)' && FOUND=1
+    if [ ${FOUND} -eq 1 ]; then
+	#Package inside MARLIN_DLL, replace string
+	export MARLIN_DLL=`echo $MARLIN_DLL | sed 's/\(:\|^\)\([^:]*\/'$pkg'\/[^:]*\)\(:\|$\)/\1'${MYBUILD}/packages/$pkg/lib/lib${pkg}.so'\3/'`
+    else
+	#Package not in MARLIN_DLL, add it
+	export MARLIN_DLL="${MYBUILD}/packages/$pkg/lib/lib${pkg}.so:${MARLIN_DLL}"
+    fi
+done
